10c80b083c91503609f05593398e7a6aa7fe0d7b
raphaeldpaiva <raphaeldpaiva@gmail.com>
2021-04-13 14:11:55 -0300

    Partitioning the game board

--- gol.py
+++ gol.py
@@ -7,6 +7,8 @@ from copy import deepcopy
 DEFAULT_BOARD_SIZE = (100, 100)      # Board size. Only square boards are supported for now.
 DEFAULT_CELL_LIFE_PROBABILITY = 0.10 # Probability of randomly generating a live cell when a new board is constructed.
 
+BoardSize_t = Tuple[int, int]
+
 # y,x or lines,columns
 DIRECTIONS = [
   (-1,-1), (-1, 0), (-1, 1),
@@ -15,20 +17,40 @@ DIRECTIONS = [
 ]
 
 class Board(object):
-  def __init__(self, size: Tuple[int, int] = DEFAULT_BOARD_SIZE, initial_state: ArrayLike = None, cell_life_probability: float = DEFAULT_CELL_LIFE_PROBABILITY):
+  def __init__(self, size: BoardSize_t = DEFAULT_BOARD_SIZE, initial_state: ArrayLike = None, cell_life_probability: float = DEFAULT_CELL_LIFE_PROBABILITY):
     if initial_state is None:
       self.size = size
       self.state = self.Random(cell_life_probability).state
     else:
       self.state = initial_state
-      self.size = (len(self.state), len(self.state))
+      self.size = (len(self.state), len(self.state[0]))
     
+    self.quadrants = self.partition(0, 0, self.size)
+  
   @staticmethod
-  def Random(size: Tuple[int, int] = DEFAULT_BOARD_SIZE, cell_life_probability: float = DEFAULT_CELL_LIFE_PROBABILITY):
+  def partition(i_start: int, j_start: int, size: BoardSize_t):
+    lines, columns = size
+    midpoint_i = int(lines / 2)
+    midpoint_j = int(columns / 2)
+
+    #----+----+
+    #  1 |  2 |
+    #----+----+
+    #  3 |  4 |
+    #----+----+
+    return [
+      (i_start, midpoint_i - 1, j_start, midpoint_j - 1), # first quadrant
+      (i_start, midpoint_i - 1, midpoint_j, columns - 1), # second quadrant
+      (midpoint_i, lines - 1,   j_start, midpoint_j - 1), # third quadrant
+      (midpoint_i, lines - 1,   midpoint_j, columns - 1)  # fourth quadrant
+    ]
+
+  @staticmethod
+  def Random(size: BoardSize_t = DEFAULT_BOARD_SIZE, cell_life_probability: float = DEFAULT_CELL_LIFE_PROBABILITY):
     p = cell_life_probability
     
     numpy.random.seed()
-    random_board = numpy.random.choice(a=[True, False], size=size, p=[p, 1 - p])
+    random_board = numpy.random.choice(a=[1, 0], size=size, p=[p, 1 - p])
     
     return Board(size, random_board)
 
@@ -37,6 +59,7 @@ class GameOfLife(object):
     self.cell_life_probability = cell_life_probability
     self.wrap = wrap_board
     self.generation_count = 0
+    self.max_depth = 3
     
     if initial_board is None:
       self.board = Board.Random(board_size, self.cell_life_probability)
@@ -47,20 +70,45 @@ class GameOfLife(object):
     new_board = deepcopy(self.board)
 
     lines, columns = self.board.size
+
+    quadrants = self.board.partition(0, 0, self.board.size)
+    for quadrant in quadrants:
+      self._analyze_quadrant(quadrant, new_board, 1)
     
-    for i in range(lines):
-      for j in range(columns):
-        cell_alive = self.board.state[i][j]
-        neighbours = self._count_neighbours(i, j)
-        if cell_alive:
-          if neighbours < 2 or neighbours > 3:
-            new_board.state[i][j] = False
-        else:
-          if neighbours == 3:
-            new_board.state[i][j] = True
     self.board = new_board
     self.generation_count += 1
+
     return self.board
+
+  def _analyze_quadrant(self, quadrant, new_board, depth):
+    i_start, i_stop, j_start, j_stop = quadrant
+    lines, columns = (i_stop - i_start) + 1, (j_stop - j_start) + 1
+    size = (lines, columns)
+    
+    if self.board.state[i_start:i_stop + 1,j_start:j_stop + 1].sum() > 1:
+      if depth <= self.max_depth:
+          self._analyze_range(quadrant, new_board)
+      else:
+        quadrants = self.board.partition(i_start, j_start, size)
+        for new_quadrant in quadrants:
+          self._analyze_quadrant(new_quadrant, new_board, depth + 1)
+
+  def _analyze_range(self, arange, new_board):
+    i_start, i_stop, j_start, j_stop = arange
+    lines, columns = (i_stop - i_start) + 1, (j_stop - j_start) + 1
+
+    for i in range(lines):
+      line = i + i_start
+      for j in range(columns):
+        column = j + j_start
+        cell_alive = self.board.state[line][column]
+        neighbours = self._count_neighbours(line, column)
+        if cell_alive:
+          if neighbours < 2 or neighbours > 3:
+            new_board.state[line][column] = 0
+        else:
+          if neighbours == 3:
+            new_board.state[line][column] = 1
 
   def _count_neighbours(self, i, j):
     neighbours = 0
